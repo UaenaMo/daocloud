80015bc871bc3ddc3b073d92ffe4145dc45c520d
jzli <jzli@scshangtong.com>
2016-08-03 15:11:08 +0800

    no message

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-phpinfo();
-?>
+$dsn = "mysql:host=tcp://10.10.26.58:3306;dbname=wfcGVXBQNdz126YL";
+$dbh = new PDO($dsn,'unBzKNg3JTrR0uxw','pAfp68HchMLEI1QFD');
+$sql = "SELECT `name` FROM `user` WHERE `id` = ?";
+$sth = $dbh->prepare($sql);
+$sth->execute(array(':id'=>1));
+$result = $sth->fetch(PDO::FETCH_OBJ);
+echo $result->name;
+$dbh = NULL;
+?>  
