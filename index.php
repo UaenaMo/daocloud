611036ae35cb97fd01d5db7b51173e28e58bf2ae
jzli <jzli@scshangtong.com>
2016-08-03 15:13:42 +0800

    no message

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$dsn = "mysql:host=tcp://10.10.26.58:3306;dbname=wfcGVXBQNdz126YL";
+$dsn = "mysql:host=10.10.26.58:3306;dbname=wfcGVXBQNdz126YL";
 $dbh = new PDO($dsn,'unBzKNg3JTrR0uxw','pAfp68HchMLEI1QFD');
 $sql = "SELECT `name` FROM `user` WHERE `id` = ?";
 $sth = $dbh->prepare($sql);
