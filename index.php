660505f8594e29ec80759e75c56219dadf6f1d92
demo234 <web_developer@qq.com>
2016-08-03 21:26:44 +0800

    Update index.php
    aaa

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<meta property="qc:admins" content="151414743354565470525417347546417100617" />
 <?php
-$dsn = "mysql:host=10.10.26.58:3306;dbname=wfcGVXBQNdz126YL";
-$dbh = new PDO($dsn,'unBzKNg3JTrR0uxw','pAfp68HchMLEI1QFD');
-$sql = "SELECT `name` FROM `user` WHERE `id` = ?";
-$sth = $dbh->prepare($sql);
-$sth->execute(array('1'));
-$result = $sth->fetch(PDO::FETCH_OBJ);
-echo $result->name;
-$dbh = NULL;
-?>  
+phpinfo();
+?>
