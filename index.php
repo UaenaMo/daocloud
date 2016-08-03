f62b5258f4f5013de57c265b8406b75b7ecc8084
jzli <jzli@scshangtong.com>
2016-08-03 15:18:09 +0800

    no message

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $dsn = "mysql:host=10.10.26.58:3306;dbname=wfcGVXBQNdz126YL";
 $dbh = new PDO($dsn,'unBzKNg3JTrR0uxw','pAfp68HchMLEI1QFD');
 $sql = "SELECT `name` FROM `user` WHERE `id` = ?";
 $sth = $dbh->prepare($sql);
-$sth->execute(array(':id'=>1));
+$sth->execute(array(1));
 $result = $sth->fetch(PDO::FETCH_OBJ);
 echo $result->name;
 $dbh = NULL;
