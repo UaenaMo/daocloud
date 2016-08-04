60843d96d91f976155cc0d4db70ab4c4869a0fbe
jzli <jzli@scshangtong.com>
2016-08-04 11:37:11 +0800

    no message

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM daocloud.io/php:7.0.0RC4-apache
+FROM daocloud.io/php:5.6.3-apache
 RUN apt-get update
 RUN docker-php-ext-install pdo_mysql
 COPY . /var/www/html/
--- index.php
+++ index.php
@@ -1,3 +1,36 @@
 <?php
-phpinfo();
+function get_client_ip($type = 0,$adv=false) {
+    $type       =  $type ? 1 : 0;
+    static $ip  =   NULL;
+    if ($ip !== NULL) return $ip[$type];
+    if($adv){
+        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $arr    =   explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+            $pos    =   array_search('unknown',$arr);
+            if(false !== $pos) unset($arr[$pos]);
+            $ip     =   trim($arr[0]);
+        }elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip     =   $_SERVER['HTTP_CLIENT_IP'];
+        }elseif (isset($_SERVER['REMOTE_ADDR'])) {
+            $ip     =   $_SERVER['REMOTE_ADDR'];
+        }
+    }elseif (isset($_SERVER['REMOTE_ADDR'])) {
+        $ip     =   $_SERVER['REMOTE_ADDR'];
+    }
+    // IP地址合法验证
+    $long = sprintf("%u",ip2long($ip));
+    $ip   = $long ? array($ip, $long) : array('0.0.0.0', 0);
+    return $ip[$type];
+}
+echo 'get_client_ip(0,true)';
+var_dump(get_client_ip(0,true));
+echo '<hr/>';
+echo 'get_client_ip(1,true)';
+var_dump(get_client_ip(1,true));
+echo '<hr/>';
+echo 'get_client_ip(0,false)';
+var_dump(get_client_ip(0,false));
+echo '<hr/>';
+echo 'get_client_ip(1,false)';
+var_dump(get_client_ip(1,false));
 ?>
