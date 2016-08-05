d0563b49cbd71574df4c514deacf7b1cb6d5a9d9
jzli <jzli@scshangtong.com>
2016-08-05 12:27:59 +0800

    no message

--- index.php
+++ index.php
@@ -1,56 +1,94 @@
 <?php
-function get_client_ip($type = 0,$adv=false) {
-    $type       =  $type ? 1 : 0;
-    static $ip  =   NULL;
-    if ($ip !== NULL) return $ip[$type];
-    if($adv){
-        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $arr    =   explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-            $pos    =   array_search('unknown',$arr);
-            if(false !== $pos) unset($arr[$pos]);
-            $ip     =   trim($arr[0]);
-        }elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
-            $ip     =   $_SERVER['HTTP_CLIENT_IP'];
-        }elseif (isset($_SERVER['REMOTE_ADDR'])) {
-            $ip     =   $_SERVER['REMOTE_ADDR'];
-        }
-    }elseif (isset($_SERVER['REMOTE_ADDR'])) {
-        $ip     =   $_SERVER['REMOTE_ADDR'];
-    }
-    // IP地址合法验证
-    $long = sprintf("%u",ip2long($ip));
-    $ip   = $long ? array($ip, $long) : array('0.0.0.0', 0);
-    return $ip[$type];
+
+require('DB.php');
+
+$db = new DB();
+
+if ($_POST) {
+    $db->add($_POST['name'], $_POST['phone']);
 }
-echo 'get_client_ip(0,true)';
-var_dump(get_client_ip(0,true));
-echo '<hr/>';
-echo 'get_client_ip(1,true)';
-var_dump(get_client_ip(1,true));
-echo '<hr/>';
-echo 'get_client_ip(0,false)';
-var_dump(get_client_ip(0,false));
-echo '<hr/>';
-echo 'get_client_ip(1,false)';
-var_dump(get_client_ip(1,false));
-
-
-/*
- *调用接口代码
- *
- **/
-require_once("./API/qqConnectAPI.php");
-$qc = new QC();
-$ret = $qc->get_info();
-
-// show result
-if($ret['ret'] == 0){
-    echo "<meta charset='utf-8' />";
-    require_once("get_info.html");
-}else{
-    echo "<meta charset='utf-8' />";
-    echo "获取失败，请开启调试查看原因";
+if ($_GET['delete']) {
+    $db->remove($_GET['delete']);
 }
 
-
+$contacts = $db->all();
 ?>
+
+<html>
+<head>
+    <title>PHP-Apache-Mysql-Sample 示例 - DaoCloud</title>
+
+    <link href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            color: #666;
+            display: table;
+        }
+
+        .container {
+            display: table-cell;
+            text-align: center;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <h1>
+            PHP-Apache-Mysql-Sample 示例
+        </h1>
+        <table class="table">
+            <caption>通讯录</caption>
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>姓名</th>
+                <th>电话</th>
+                <th>操作</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($contacts as $index => $contact) {
+                ?>
+                <tr>
+                    <th scope="row"> <?php echo $index + 1 ?></th>
+                    <td><?php echo $contact['name'] ?></td>
+                    <td><?php echo $contact['phone'] ?></td>
+                    <td>
+                        <a href="index.php?delete=<?php echo $contact['id'] ?>">删除</a>
+                    </td>
+                </tr>
+            <?php
+            } ?>
+            </tbody>
+        </table>
+
+        <div class="text-left">
+            <form method="post">
+                <div class="form-group">
+                    <label for="name">姓名</label>
+                    <input type="text" name="name" class="form-control" id="name" placeholder="姓名">
+                </div>
+                <div class="form-group">
+                    <label for="phone">号码</label>
+                    <input type="text" name="phone" class="form-control" id="phone" placeholder="号码">
+                </div>
+                <button type="submit" class="btn btn-success">新增</button>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
